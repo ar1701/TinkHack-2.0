98d4cd3c5795eb82e6756301318d80b3de641449
Ayush Raj <ar898993@gmail.com>
2025-03-23 06:29:37 +0530

    Chat bug fixed

--- models/chatMessage.js
+++ models/chatMessage.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const ChatMessageSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const chatMessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("ChatMessage", ChatMessageSchema);
+module.exports = mongoose.model("ChatMessage", chatMessageSchema);
